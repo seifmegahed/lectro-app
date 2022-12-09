ecf563b21193d57ab0a63f074656e66afd7705d2
Seif <seif@Seif-3.local>
2022-12-09 20:24:44 +0200

    replaced DataGrid table with new <MainTable /> component in invoices scene

--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -1,20 +1,10 @@
-import { Box, Typography, useTheme } from "@mui/material";
-import { DataGrid , GridToolbar } from "@mui/x-data-grid";
-import { tokens } from "../../theme";
+import { Box } from "@mui/material";
 import { mockDataInvoices } from '../../data/mockData'
 
-import { 
-  AdminPanelSettingsOutlined as AdminPanelSettingsOutlinedIcon ,
-  LockOpenOutlined as LockOpenOutlinedIcon ,
-  SecurityOutlined as SecurityOutlinedIcon 
-} from "@mui/icons-material";
-
 import Header from "../../components/Header";
+import MainTable from "../../components/mainTable";
 
 const Invoices = () => {
-  const theme = useTheme()
-  const colors = tokens(theme.palette.mode)
-
   const columns = [
     {
       field: "id", headerName: "ID",
@@ -52,52 +42,10 @@ const Invoices = () => {
   return (
     <Box m="20px">
       <Header title="INVOICES" subtitle="List of Invoice Balances" />
-      <Box
-        m="40px 0 0 0"
-        height="95vh"
-        backgroundColor= {colors.primary[700]}
-        borderRadius= "5px"
-        sx={{
-          "& .MuiDataGrid-root" : {
-            border: "none"
-          } ,
-          "& .MuiDataGrid-cell" : {
-            borderBottom: "none"
-          } ,
-          "& .MuiDataGrid-virtualScroller" : {
-            transform: "none"
-          },
-          "& .MuiDataGrid-footerContainer" : {
-            borderTop: "none",
-          },
-          "& .MuiDataGrid-toolbarContainer .MuiButton-text" : {
-            color: `${colors.grey[100]} !important`,
-          },
-          "& .MuiCheckbox-root" : {
-            color: `${colors.grey[200]} !important`,
-          },
-
-        }}
-      >
-        <DataGrid 
-          rows={mockDataInvoices}
-          columns={columns}
-          disableDensitySelector
-          disableColumnFilter
-          checkboxSelection
-          components={{ Toolbar: GridToolbar}}
-          componentsProps={{
-            toolbar: {
-              csvOptions: { disableToolbarButton: true },
-              printOptions: { disableToolbarButton: true },
-              showQuickFilter: true,
-              quickFilterProps: { debounceMs: 500 },
-            },
-          }}
-          autoPageSize
-          pagination
-        />
-      </Box>
+      <MainTable 
+        columns={columns}
+        data={mockDataInvoices}
+      />
     </Box>
   )
 }
