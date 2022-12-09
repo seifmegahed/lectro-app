e5c2d1fc5a4f75ce907180efe801e71235408ef5
Seif <seif@Seif-3.local>
2022-12-09 20:23:39 +0200

    replaced DataGrid table with new <MainTable /> component in contacts scene

--- src/scenes/contacts/index.jsx
+++ src/scenes/contacts/index.jsx
@@ -1,21 +1,10 @@
-import { Box , useTheme } from "@mui/material";
-import { DataGrid , GridToolbar } from "@mui/x-data-grid";
-import { tokens } from "../../theme";
+import { Box } from "@mui/material";
 import { mockDataContacts } from '../../data/mockData'
 
-
-import { 
-  AdminPanelSettingsOutlined as AdminPanelSettingsOutlinedIcon ,
-  LockOpenOutlined as LockOpenOutlinedIcon ,
-  SecurityOutlined as SecurityOutlinedIcon 
-} from "@mui/icons-material";
-
 import Header from "../../components/Header";
+import MainTable from "../../components/mainTable";
 
 const Contacts = () => {
-  const theme = useTheme()
-  const colors = tokens(theme.palette.mode)
-
   const columns = [
     {
       field: "id", headerName: "ID",
@@ -51,56 +40,10 @@ const Contacts = () => {
   return (
     <Box m="20px">
       <Header title="CONTACTS" subtitle="List of Contacts for Future Referance" />
-      <Box
-        mt="40px"
-        p="10px"
-        height="95vh"
-        backgroundColor= {colors.primary[700]}
-        borderRadius= "5px"
-        sx={{
-          "& .MuiDataGrid-root" : {
-            border: "none"
-          } ,
-          "& .MuiDataGrid-cell" : {
-            borderBottom: "none"
-          } ,
-          "& .name-column--cell" : {
-          },
-          "& .MuiDataGrid-columnHeaders" : {
-          },
-          "& .MuiDataGrid-virtualScroller" : {
-            transform: "none"
-          },
-          "& .MuiDataGrid-footerContainer" : {
-            borderTop: "none",
-          },
-          "& .MuiDataGrid-toolbarContainer .MuiButton-text" : {
-            color: `${colors.grey[100]} !important`,
-          },
-          "& .MuiDataGrid-toolbarContainer .MuiButtonBase-MuiButton-root" : {
-            color: `${colors.grey[100]} !important`,
-          }
-        }}
-      >
-        <DataGrid 
-          rows={mockDataContacts}
-          columns={columns}
-          disableDensitySelector
-          disableColumnFilter
-          components={{ Toolbar: GridToolbar}}
-          componentsProps={{
-            toolbar: {
-              csvOptions: { disableToolbarButton: true },
-              printOptions: { disableToolbarButton: true },
-              showQuickFilter: true,
-              quickFilterProps: { debounceMs: 500 },
-            },
-          }}
-          autoPageSize
-          pagination
-        />
-        
-      </Box>
+      <MainTable 
+        columns={columns}
+        data={mockDataContacts}
+      />
     </Box>
   )
 }
