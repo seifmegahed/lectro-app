4399c10c701a25f0afb7a2298fe926ad8bf23f10
Seif Megahed <seifmegahed@me.com>
2023-01-09 21:53:41 +0200

    react-query and firebase-query dep

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React, { lazy, Suspense } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import { ColorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider, Box, Typography, useMediaQuery } from "@mui/material";
+import {
+  CssBaseline,
+  ThemeProvider,
+  Box,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
 
 import Topbar from "./components/Topbar";
 import Loading from "./components/Loading";
 
 import { useAuth } from "./contexts/AuthContext";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const NewProject = lazy(() => import("./scenes/projects/newProject"));
 const Dashboard = lazy(() => import("./scenes/dashboard"));
@@ -19,53 +26,57 @@ const Suppliers = () => <Accounts type="Supplier" />;
 const Clients = () => <Accounts type="Client" />;
 
 function App() {
-  const isNonMobile = useMediaQuery("(min-width:600px)")
+  const isNonMobile = useMediaQuery("(min-width:600px)");
   const [theme, colorMode] = useMode();
   const { currentUser } = useAuth();
+
+  const queryClient = new QueryClient();
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Box className="app" display="flex">
-        <Topbar />
-        {!!currentUser ? (
-          <Box
-            className="content"
-            pt="85px"
-            m={isNonMobile ? "40px" : "20px"}
-            maxWidth="700px"
-            position="relative"
-          >
-            <Suspense fallback={<Loading />}>
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/clients" element={<Clients />} />
-                <Route path="/new-project" element={<NewProject />} />
-                <Route path="/items" element={<Inventory />} />
-                <Route path="/suppliers" element={<Suppliers />} />
-                <Route path="/*" element={<Navigate to="/" />} />
-              </Routes>
-            </Suspense>
-          </Box>
-        ) : (
-          <Routes>
-            <Route
-              path="*"
-              element={
-                <Box
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                  padding="150px"
-                >
-                  <Typography variant="h2">Welcome!</Typography>
-                </Box>
-              }
-            />
-          </Routes>
-        )}
+          <Topbar />
+          {!!currentUser ? (
+            <Box
+              className="content"
+              pt="85px"
+              m={isNonMobile ? "40px" : "20px"}
+              maxWidth="700px"
+              position="relative"
+            >
+              <Suspense fallback={<Loading />}>
+                <QueryClientProvider client={queryClient}>
+                  <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/clients" element={<Clients />} />
+                    <Route path="/new-project" element={<NewProject />} />
+                    <Route path="/items" element={<Inventory />} />
+                    <Route path="/suppliers" element={<Suppliers />} />
+                    <Route path="/*" element={<Navigate to="/" />} />
+                  </Routes>
+                </QueryClientProvider>
+              </Suspense>
+            </Box>
+          ) : (
+            <Routes>
+              <Route
+                path="*"
+                element={
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    padding="150px"
+                  >
+                    <Typography variant="h2">Welcome!</Typography>
+                  </Box>
+                }
+              />
+            </Routes>
+          )}
         </Box>
       </ThemeProvider>
     </ColorModeContext.Provider>
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,23 +1,25 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "@firebase/firestore"
+import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
-const { initializeAppCheck, ReCaptchaV3Provider } = require("firebase/app-check");
-
+const {
+  initializeAppCheck,
+  ReCaptchaV3Provider,
+} = require("firebase/app-check");
 
 const firebaseConfig = {
-  apiKey:             process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain:         process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId:          process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket:      process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId:  process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId:              process.env.REACT_APP_FIREBASE_APPID,
-}
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+};
 
-export const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 export const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider('6LdLM6MjAAAAAEStPp3orS_ErpmS-484hyA4kmWx'),
-  isTokenAutoRefreshEnabled: true
+  provider: new ReCaptchaV3Provider("6LdLM6MjAAAAAEStPp3orS_ErpmS-484hyA4kmWx"),
+  isTokenAutoRefreshEnabled: true,
 });
