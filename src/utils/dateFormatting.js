757f2664769cfb9ca83aee144f7c6ab0f8bd8c22
Seif Megahed <seifmegahed@me.com>
2023-01-09 21:47:38 +0200

    fixed issue with date formating

--- src/utils/dateFormatting.js
+++ src/utils/dateFormatting.js
@@ -18,7 +18,7 @@ export function getFormatedDate(val) {
   let date = 0;
   !!val.nanoseconds
     ? (date = new Date(val.seconds * 1000))
-    : (date = new Date(val.seconds));
+    : (date = new Date(val));
 
   const month = months[date.getMonth()];
   const day = date.getDate();
